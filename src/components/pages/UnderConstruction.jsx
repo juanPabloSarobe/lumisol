7e1183cd8b260a2ea72508b0b50b6bb4e1b06580
Juan Pablo Sarobe <jp.sarobe@gmail.com>
2024-05-22 14:27:41 -0300

    version 1.1 en construccion

--- src/components/pages/UnderConstruction.jsx
+++ src/components/pages/UnderConstruction.jsx
@@ -6,34 +6,34 @@ const underConstruction = () => {
       id="container"
       className=" mt-5 lg:mt-0 ml-2 mr-2 md:w-vw md:h-vh bg-white dark:bg-slate-700 flex  items-center justify-center "
     >
-      <div className="lg:w-2/4 lg:h-2/5 flex flex-col justify-center items-center ">
-        <div className=" self-end bg-red-400 absolute  top-20 center-10 animate__animated animate__swing animate__slow ">
+      <div className=" flex flex-col justify-center items-center ">
+        <div className=" self-end bg-red-400 absolute top-20 right-[15%]  animate__animated animate__swing animate__slow ">
           <img
-            src="..\..\src\assets\underConstruction.png "
+            src="..\..\underConstruction.png "
             alt="Conection image"
             className="w-20 bg-red-400 "
           />
         </div>
         <div className=" mb-5 flex flex-row justify-center items-center bg-white rounded-lg">
           <img
-            src="..\..\src\assets\Logo_lumisol.png "
+            src="..\..\Logo_lumisol.png "
             alt="Conection image"
             className="w-10  rounded-lg"
           />
           <img
-            src="..\..\src\assets\Nombre_lumisol.png "
+            src="..\..\Nombre_lumisol.png "
             alt="Conection image"
             className="h-10  rounded-lg"
           />
         </div>
         <img
-          src="..\..\src\assets\connect.webp "
+          src="..\..\connect.webp "
           alt="Conection image"
           className="w-3/5 rounded-lg"
         />
         <h2 className="text-2xl mt-3 text-center dark:text-slate-200 text-slate-800 ">
-          En breve te conectaremos con un alternativa sustentable, asequible y
-          limpia de energia.!!!!
+          En breve te conectaremos a una energía sustentable, asequible y
+          limpia.!!!!
         </h2>
       </div>
     </div>
